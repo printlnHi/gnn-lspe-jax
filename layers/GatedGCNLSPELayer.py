3416ed20b14c41a346c45708b445958cbec692ad
Marcus Handley <printlnHi@users.noreply.github.com>
2023-04-20 21:19:05 +0100

    fixed snorm bug

--- layers/GatedGCNLSPELayer.py
+++ layers/GatedGCNLSPELayer.py
@@ -108,7 +108,6 @@ class GatedGCNLSPELayer(hk.Module):
     node_layer_features *= nodes['snorm_n'][:, None]
     HaikuDebug("node_before", enable=debug)(node_layer_features)
 
-    h *= nodes['snorm_n'][:, None]
     if self.mask_batch_norm:
       node_mask = jraph.get_node_padding_mask(graph)
       node_layer_features = masked.BatchNorm(
