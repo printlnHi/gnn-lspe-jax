b4fb29712d0ca24298e614114a47a5e5e0f46acf
Marcus Handley <printlnHi@users.noreply.github.com>
2023-05-08 04:22:05 +0100

    Masked move

--- lib/masked.py
+++ lib/masked.py
@@ -1,22 +1,4 @@
-# Copyright 2019 DeepMind Technologies Limited. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-
 # TODO: Document module
-# TODO: Adapt comment to reflect the fact that this is a fork of Haiku's BatchNorm, and only for the batchnorm
-# module. Or, realiase that as I'm using Haiku the whole project is a derivate work so I just have to do a single license
-# + an acknowledgment here
 import types
 from typing import Optional, Sequence, Union
 
@@ -51,6 +33,21 @@ def mean(a: jnp.ndarray, mask: ArrayLike, axis=None, keepdims=False) -> Array:
 
 masked.mean = mean
 
+
+# Copyright 2019 DeepMind Technologies Limited. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 
 class BatchNorm(hk.Module):
   """Normalizes inputs to maintain a mean of ~0 and stddev of ~1.
--- modules/gatedgcn.py
+++ modules/gatedgcn.py
@@ -6,7 +6,7 @@ import jax.numpy as jnp
 import jax.tree_util as tree
 import jraph
 
-import lib.masked as masked
+import modules.masked as masked
 from lib.debug import HaikuDebug
 
 
